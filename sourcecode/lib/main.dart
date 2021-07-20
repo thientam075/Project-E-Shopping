d402396f10fd6917cb3ef75c562800f0a79d5109
I99900K <46591640+I99900K@users.noreply.github.com>
2021-07-20 08:59:48 +0700

    Add files via upload

--- sourcecode/lib/main.dart
+++ sourcecode/lib/main.dart
@@ -1,25 +1,46 @@
+import 'package:e_shopping_application/AllScreens/loginScreen.dart';
+import 'package:e_shopping_application/AllScreens/mainScreen.dart';
+import 'package:e_shopping_application/AllScreens/mainview.dart';
+import 'package:e_shopping_application/AllScreens/registerScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+DatabaseReference usersRef = FirebaseDatabase.instance.reference().child("users");
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Home',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Home-Page'),
-        ),
-      ),
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "Brand-Bold",
+      ),
+      initialRoute: LoginScreen.idScreen,
+      routes: {
+        RegisterScreen.idScreen: (context) => RegisterScreen(),
+        LoginScreen.idScreen: (context) => LoginScreen(),
+        MainScreen.idScreen: (context) => MainView(),
+      },
     );
   }
 }
-
